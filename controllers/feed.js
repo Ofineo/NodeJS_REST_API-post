cdc68fcaaddf717e0f7bfe533d9f7469b679523b
Jordi <ofineo@gmail.com>
2020-07-04 20:23:55 +0100

    delete to all clients with socker io

--- controllers/feed.js
+++ controllers/feed.js
@@ -164,6 +164,7 @@ exports.deletePost = async (req, res, next) => {
     const user = await User.findById(req.userId);
     user.posts.pull(postId);
     await user.save();
+    io.getIO().emit("posts", { action: "delete", posts: postId });
     res.status(200).json({ message: "deleted post" });
   } catch (err) {
     if (!err.statusCode) {
