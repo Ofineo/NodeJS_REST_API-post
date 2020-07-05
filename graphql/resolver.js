20eee23d22339bb4b96963a26b92fc44b5ceffda
Jordi <ofineo@gmail.com>
2020-07-05 13:56:30 +0100

    sign in with graphql

--- graphql/resolver.js
+++ graphql/resolver.js
@@ -1,5 +1,22 @@
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+
 module.exports = {
-    hello(){
-        return {text:'the query is working',views:1234}
+  createUser: async function ({ userInput }, req) {
+    // const email = args.userInput.email;
+    const existingUser = await User.findOne({ email: userInput.email });
+    if (existingUser) {
+      const error = new Error("this email is assigned to a user already");
+      error.statusCode = 401;
+      throw error;
     }
-}
+    const hashedPassword = await bcrypt.hash(userInput.password, 12);
+    const user = new User({
+      email: userInput.email,
+      name: userInput.name,
+      password: hashedPassword,
+    });
+    const newUser = await user.save();
+    return { ...newUser._doc, _id: newUser._id.toString() };
+  },
+};
